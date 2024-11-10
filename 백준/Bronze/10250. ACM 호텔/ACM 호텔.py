f47d7f2aaeda6db9fb8a64a27cb61cfdb6c08ed7
1ska2suwkd <mickey050110@naver.com>
2024-11-10 10:07:36 +0000

    [Bronze III] Title: ACM 호텔, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -4,12 +4,16 @@ t = int(input())
 for _ in range(t):
     cnt=0
     hotel = []
-    h,w,n = map(int,input().split())
+    h,w,n = map(int,input().split())\
+        
+    #호텔의 크기에 맞는 객실 번호를 매기는 반복문
     for i in range(1,h+1):
         hotel_w = []
         for j in range(1,w+1):
             hotel_w.append(i*100+j)
         hotel.append(hotel_w)
+        
+    #주어진 규칙에 맞는 n번째 손님 방을 찾는 반복문 
     for i in range(w):
         for j in range(h):
             cnt+=1
