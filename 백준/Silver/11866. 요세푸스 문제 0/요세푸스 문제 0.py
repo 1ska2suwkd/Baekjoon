0792d4cb5e3fe17aa5f1867374849da07850d53f
1ska2suwkd <mickey050110@naver.com>
2025-06-22 15:12:07 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,12 +1,11 @@
 N,K = map(int,input().split())
-people = list(range(1,N+1))
-index = K-1
-result = []
 
-while len(people):
-    result.append(people[index])
-    del people[index]
-    if len(people) != 0:
-        index = (index+K-1)%len(people)
+people = list(range(1,N+1))
+result = []
+index = 0
+
+while people:
+    index = (index+K-1)%len(people)
+    result.append(people.pop(index))
 
 print("<"+", ".join(map(str,result))+">")
